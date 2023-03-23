e0648fefe095f7ef465fbeef1df1d97bdd5f45f0
aalexandr6 <aalexanderp6@gmail.com>
2023-03-23 17:17:59 -0500

    updating server

--- Develop/server.js
+++ Develop/server.js
@@ -10,3 +10,18 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use(session(sess))
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening on port 3001'));
+});
+
