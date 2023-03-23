db2add813f4179f9eda5871dc8232908a3fd1a72
aalexandr6 <aalexanderp6@gmail.com>
2023-03-23 17:42:39 -0500

    Develop/server.js

--- Develop/server.js
+++ Develop/server.js
@@ -11,6 +11,8 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const hbs = exphbs.create({ helpers });
+
 app.use(session(sess))
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
