5350746d05ff150d5cf5f5338eaab4c6fe2ece01
aalexandr6 <aalexanderp6@gmail.com>
2023-03-23 21:31:20 -0500

    frostbite

--- Develop/tailwind.config.js
+++ Develop/tailwind.config.js
@@ -7,3 +7,17 @@ module.exports = {
   },
   plugins: [],
 }
+module.exports = {
+
+  plugins: [
+      require('flowbite/plugin')
+  ]
+
+}
+module.exports = {
+
+  content: [
+      "./node_modules/flowbite/**/*.js"
+  ]
+
+}
