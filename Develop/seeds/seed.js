9944a10c6fe969378213e2a1c8ab13ac21e385d0
aalexandr6 <aalexanderp6@gmail.com>
2023-03-30 17:11:25 -0500

    seeds

--- Develop/seeds/seed.js
+++ Develop/seeds/seed.js
@@ -1,74 +1,32 @@
-const express = require('express');
-const mysql = require('mysql2');
+const {user} = require('../models/user');
+const {data} = require('../models/data');
+const sequelize = require('../config/connection');
+const userData = require('./userData.json');
+const driverData = require('./driverData.json');
 
-const PORT = process.env.PORT || 3001;
-const app = express();
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true })
+};
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-const db = mysql.createConnection(
-  {
-    host: 'localhost',
-    user: 'root',
-    password: 'xh1394jln',
-    database: 'drivers_db'
-  },
-  console.log(`Connected to the drivers_db database.`)
-);
-
-
-app.post('/api/new-driver', ({ body }, res) => {
-  const sql = `INSERT INTO drivers (driver_name)
-    VALUES (?)`;
-  const params = [body.driver_name];
-  
-  db.query(sql, params, (err, result) => {
-    if (err) {
-      res.status(400).json({ error: err.message });
-      return;
-    }
-    res.json({
-      message: 'success',
-      data: body
-    });
-  });
+//login info
+const users = await user.bulkCreate(userData, {
+  individualHooks: true,
+  returning: true,
 });
 
-
-app.get('/api/drivers', (req, res) => {
-  const sql = `SELECT id, driver_name AS name FROM drivers`;
-  
-  db.query(sql, (err, rows) => {
-    if (err) {
-      res.status(500).json({ error: err.message });
-       return;
-    }
-    res.json({
-      message: 'success',
-      data: rows
-    });
-  });
+//driver info
+const drivers = await data.bulkCreate(driverData, {
+  individualHooks: true,
+  returning: true,
 });
 
-
-app.delete('/api/driver/:id', (req, res) => {
-  const sql = `DELETE FROM drivers WHERE id = ?`;
-  const params = [req.params.id];
-  
-  db.query(sql, params, (err, result) => {
-    if (err) {
-      res.statusMessage(400).json({ error: res.message });
-    } else if (!result.affectedRows) {
-      res.json({
-      message: 'Driver not found'
-      });
-    } else {
-      res.json({
-        message: 'deleted',
-        changes: result.affectedRows,
-        id: req.params.id
-      });
-    }
+for (const data of driverData) {
+  await data.create({
+    ...data,
+    user_id: users[Math.floor(Math.random() * users.length)].id,
   });
-});
+}
+
+process.exit(0);
+
+seedDatabase();
