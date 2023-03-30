6fefbf664d32b38afbc15319b8c4b655ba3983f3
aalexandr6 <aalexanderp6@gmail.com>
2023-03-30 17:13:50 -0500

    duplicate

--- Develop/controllers/api/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const useroutes = require('./user-routes');
-const dataroutes = require('./data-routes');
-
-router.use('/users', useroutes);
-router.use('/data', dataroutes);
-
-module.exports = router;
