523ec2dc737b229125085a8a258652c039813a23
aalexandr6 <aalexanderp6@gmail.com>
2023-04-03 16:02:15 -0500

    update index/homep/readme

--- Develop/controllers/index.js
+++ Develop/controllers/index.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const apiroutes = require('./api');
+const homeroutes=require('./home-routes')
 
 router.use ('/api', apiroutes);
-
+router.use ('/',homeroutes);
 
 module.exports = router;
